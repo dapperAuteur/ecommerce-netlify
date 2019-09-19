701b7ab49635b30c9e595754f00411bdc8f18b66
Anthony McDonald <a@awews.com>
2019-09-18 18:16:39 -0700

    test endpoint by reverting to simple func in functions/index

--- store/index.js
+++ store/index.js
@@ -1,78 +1,78 @@
-import axios from 'axios'
-import uuidv1 from 'uuid/v1'
-import data from '~/static/storedata.json'
+import axios from "axios";
+import uuidv1 from "uuid/v1";
+import data from "~/static/storedata.json";
 
 export const state = () => ({
-  cartUIStatus: 'idle',
+  cartUIStatus: "idle",
   storedata: data,
   cart: []
-})
+});
 
 export const getters = {
-  featuredProducts: (state) => state.storedata.slice(0, 3),
-  women: (state) => state.storedata.filter((el) => el.gender === 'Female'),
-  men: (state) => state.storedata.filter((el) => el.gender === 'Male'),
-  cartCount: (state) => {
-    if (!state.cart.length) return 0
-    return state.cart.reduce((ac, next) => ac + next.quantity, 0)
+  featuredProducts: state => state.storedata.slice(0, 3),
+  women: state => state.storedata.filter(el => el.gender === "Female"),
+  men: state => state.storedata.filter(el => el.gender === "Male"),
+  cartCount: state => {
+    if (!state.cart.length) return 0;
+    return state.cart.reduce((ac, next) => ac + next.quantity, 0);
   },
-  cartTotal: (state) => {
-    if (!state.cart.length) return 0
-    return state.cart.reduce((ac, next) => ac + next.quantity * next.price, 0)
+  cartTotal: state => {
+    if (!state.cart.length) return 0;
+    return state.cart.reduce((ac, next) => ac + next.quantity * next.price, 0);
   }
-}
+};
 
 export const mutations = {
   updateCartUI: (state, payload) => {
-    state.cartUIStatus = payload
+    state.cartUIStatus = payload;
   },
-  clearCart: (state) => {
+  clearCart: state => {
     // this clears the cart
-    ;(state.cart = []), (state.cartUIStatus = 'idle')
+    (state.cart = []), (state.cartUIStatus = "idle");
   },
   addToCart: (state, payload) => {
-    const itemfound = state.cart.find((el) => el.id === payload.id)
+    const itemfound = state.cart.find(el => el.id === payload.id);
     itemfound
       ? (itemfound.quantity += payload.quantity)
-      : state.cart.push(payload)
+      : state.cart.push(payload);
   }
-}
+};
 
 export const actions = {
   async postStripeFunction({ getters, commit }, payload) {
-    commit('updateCartUI', 'loading')
+    commit("updateCartUI", "loading");
 
     try {
       await axios
         .post(
-          'https://ecommerce-netlify.netlify.com/.netlify/functions/index',
+          "https://fervent-curie-071308.netlify.com/.netlify/functions/index",
           {
             stripeEmail: payload.stripeEmail,
             stripeAmt: Math.floor(getters.cartTotal * 100), // it expects the price in cents, as an integer
-            stripeToken: 'tok_visa', // testing token, later we would use payload.data.token
+            stripeToken: "tok_visa", // testing token, later we would use payload.data.token
             stripeIdempotency: uuidv1() // we use this library to create a unique id
           },
           {
             headers: {
-              'Content-Type': 'application/json'
+              "Content-Type": "application/json"
             }
           }
         )
-        .then((res) => {
+        .then(res => {
           if (res.status === 200) {
-            commit('updateCartUI', 'success')
-            setTimeout(() => commit('clearCart'), 3000)
+            commit("updateCartUI", "success");
+            setTimeout(() => commit("clearCart"), 3000);
           } else {
-            commit('updateCartUI', 'failure')
+            commit("updateCartUI", "failure");
             // allow them to try again
-            setTimeout(() => commit('updateCartUI', 'idle'), 3000)
+            setTimeout(() => commit("updateCartUI", "idle"), 3000);
           }
 
-          console.log(JSON.stringify(res, null, 2))
-        })
+          console.log(JSON.stringify(res, null, 2));
+        });
     } catch (err) {
-      console.log(err)
-      commit('updateCartUI', 'failure')
+      console.log(err);
+      commit("updateCartUI", "failure");
     }
   }
-}
+};
